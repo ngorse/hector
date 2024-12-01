4045676531361d51ee683612dcb0cb5bd11cdde4
Nicolas Gorse <nicolas.gorse@ulex.ca>
2024-12-01 01:38:11 +0100

    changed port to 5555

--- server.py
+++ server.py
@@ -27,4 +27,4 @@ def get_bot_response():
     return get_answer(userText)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
