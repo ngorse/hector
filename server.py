d4a41af5309db55f5d70bc392bdbb9c578c297e5
Nicolas Gorse <nicolas.gorse@ulex.ca>
2024-12-02 12:15:18 +0100

    added python error handling and chat capability

--- server.py
+++ server.py
@@ -1,20 +1,38 @@
 import json
 import requests
+from datetime import datetime
 from flask import Flask, render_template, request
 
-URL = "http://localhost:11434/api/generate"
+URL = "http://localhost:11434/api/chat"
 app = Flask(__name__)
 
 def get_answer(prompt, model="openchat", stream=False):
     input = {
         "model": model,
-        "prompt": prompt,
-        "stream": stream
+        "stream": stream,
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
     }
 
-    output = requests.post(URL, json=input)
-    response = json.loads(output.text)["response"]
-    print(response)
+    print(f"\nDATE  : {datetime.now()}")
+    print(f"INPUT : {prompt}")
+    try:
+        output = requests.post(URL, json=input)
+        response = json.loads(output.text)["message"]["content"]
+    except json.JSONDecodeError as e:
+        response = f"[SYSTEM ERROR]: Error decoding JSON: {e}"
+    except KeyError as e:
+        response = f"[SYSTEM ERROR]: Key error accessing response data: {e}"
+    except requests.exceptions.RequestException as e:
+        response = f"[SYSTEM ERROR]: An error occurred while making the POST request: {e}"
+    except Exception as e:
+        response = f"[SYSTEM ERROR]: An unexpected error occurred: {e}"
+
+    print(f"OUTPUT:{response}\n")
     return response.replace("\n", "<br>")
 
 @app.route("/")
